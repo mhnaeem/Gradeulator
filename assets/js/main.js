1c90d71b8d12834680fc30ae0aad4e889a326d5f
Muhammad Hammad <mhnaeem@mun.ca>
2019-09-17 00:31:37 -0230

    added the object type for marked coursework

--- assets/js/main.js
+++ assets/js/main.js
@@ -74,7 +74,7 @@ class Course{
     
     constructor(name){
         this.courseName = name;
-        this.markedWork = [];
+        this.markedStuff = [];
         this.courseNum = Course.staticNum;
         Course.staticNum += 1;
     }
@@ -86,6 +86,28 @@ class Course{
     
     crInfo(){
         return '<li class="nav-item"><a class="nav-link" href="#" onclick="selectCr('+this.courseNum+')"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg> <span data-feather="file-text"></span>' + this.courseName + '</a> </li>';
+    }
+    
+    addMarkedWork(nm, ttlMarks, recMarks, weight){
+        var obj = {
+            "name" : nm,
+            "totalMarks" : ttlMarks,
+            "marksReceived" : recMarks,
+            "weightage" : weight,
+            "weightageReceived" : this.marksReceived / this.totalMarks * this.weightage;
+        };
+        this.markedStuff.push(obj);
+    }
+    
+    removeMarkedStuff(nm){
+        for (var i = 0; i < this.markedStuff.length; i++){
+            if(this.markedStuff.name == nm){
+                var index = this.markedStuff.indexOf(this.markedStuff[i]);
+                if (index > -1) {
+                  this.markedStuff.splice(index, 1);
+                }
+            }
+        }
     }
 }
 
