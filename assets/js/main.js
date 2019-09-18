1fd995ae83c6701aa1d74757dbf12f61f90e52ef
Muhammad Hammad <mhnaeem@mun.ca>
2019-09-17 22:10:47 -0230

    Added a pie chart for showing the entire course weight, added the modify and remove buttons for the marked work inside courses.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+//Student Class Complete
 class Student{
     constructor (usrNm, pass){
         this.userName = usrNm;
@@ -21,11 +22,11 @@ class Student{
                 }
             }
         }
-        console.log(this.sems);
     }
 }
+//Student Class Complete
 
-
+//Semester class complete
 class Semester{
 
     constructor(semNm){
@@ -64,12 +65,14 @@ class Semester{
                 }
             }
         }
-        console.log(this.courses);
     }    
 }
+//Semester class complete
 
+//Unique number for each semester
 Semester.staticNum = 0;
 
+// Course class complete
 class Course{
     
     constructor(name){
@@ -88,7 +91,7 @@ class Course{
         return '<li class="nav-item"><a class="nav-link" href="#" onclick="selectCr('+this.courseNum+')"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg> <span data-feather="file-text"></span>' + this.courseName + '</a> </li>';
     }
     
-    addMarkedWork(nm, ttlMarks, recMarks, weight){
+    addMarkedWork(nm = '', ttlMarks = 0.0, recMarks = 0.0, weight = 0.0){
         var obj = {
             "name" : nm,
             "totalMarks" : ttlMarks,
@@ -97,23 +100,11 @@ class Course{
             "weightageReceived" : recMarks / ttlMarks * weight
         };
         this.markedStuff.push(obj);
-        console.log(this.markedStuff);
-    }
-    
-    addMarkedWorkWithoutReceived(nm, ttlMarks, weight){
-        var obj = {
-            "name" : nm,
-            "totalMarks" : ttlMarks,
-            "weightage" : weight,
-            "weightageReceived" : recMarks / ttlMarks * weight
-        };
-        this.markedStuff.push(obj);
-        console.log(this.markedStuff);
     }
     
     removeMarkedStuff(nm){
         for (var i = 0; i < this.markedStuff.length; i++){
-            if(this.markedStuff.name == nm){
+            if(this.markedStuff[i].name == nm){
                 var index = this.markedStuff.indexOf(this.markedStuff[i]);
                 if (index > -1) {
                   this.markedStuff.splice(index, 1);
@@ -129,8 +120,14 @@ class Course{
             }
         });
     }
+    
+    getMarkedWorkInfo(currMW){
+        return '<tr><td>' + currMW.name + '</td><td>' + currMW.totalMarks + '</td><td>' + currMW.marksReceived + '</td><td>' + currMW.weightage + '</td><td>' + currMW.weightageReceived + '</td><td>' + '<button onclick="modifyMarkedWork(\'' + currMW.name + '\')">Modify</button><button onclick="removeMarkedWork(\'' + currMW.name + '\')">Remove</button>' + '</td></tr>';
+    }
 }
+//Course class complete
 
+//Unique number for each course
 Course.staticNum = 0;
 
 
@@ -138,17 +135,18 @@ var stu = new Student('hh','hh');
 var currSem;
 var currCourse;
 
-
+//Select a semester complete
 function selectSem(n){
-    //For changing the active semester
-    /*
-    var semBar = document.getElementById("semesters");
-    var temp = "";
-    if (currSem != null){
-        temp = currSem.semInfo().replace('class="nav-link active"', 'class="nav-link"');
-        semBar.innerHTML = semBar.innerHTML.replace(currSem.semInfo(),temp);
+    
+    var semBarSems = document.getElementById("semesters").getElementsByTagName("a");
+    
+    for(var i = 0; i < semBarSems.length; i++){
+        var test = "nav-link active";
+        if(semBarSems[i].getAttribute("class") == test){
+            semBarSems[i].setAttribute("class", "nav-link");
+        }
     }
-    */
+    
     for(var i = 0; i < stu.sems.length; i++){
         if (stu.sems[i].semNum == n){
             currSem = stu.sems[i];
@@ -161,20 +159,45 @@ function selectSem(n){
             bar.innerHTML += currSem.courses[i].crInfo();
         }
     }
-    console.log(currSem.courses);
-    //For changing the active semester
-    /*temp = currSem.semInfo().replace('class="nav-link"', 'class="nav-link active"');
-    semBar.innerHTML = semBar.innerHTML.replace(currSem.semInfo(),temp);*/
+    
+    for(var i = 0; i < semBarSems.length; i++){
+        var test = "selectSem(" + n + ")";
+        if(semBarSems[i].getAttribute("onclick") == test){
+            semBarSems[i].setAttribute("class", "nav-link active");
+        }
+    }
 }
+//Select a semester complete
 
+//Select a course complete
 function selectCr(n){
+    
+    var crsBarCrs = document.getElementById("courses").getElementsByTagName("a");
+    
+    for(var i = 0; i < crsBarCrs.length; i++){
+        var test = "nav-link active";
+        if(crsBarCrs[i].getAttribute("class") == test){
+            crsBarCrs[i].setAttribute("class", "nav-link");
+        }
+    }
+    
     for(var i = 0; i < currSem.courses.length; i++){
         if (currSem.courses[i].courseNum == n){
             currCourse = currSem.courses[i];
         }
     }
+    
+    
+    for(var i = 0; i < crsBarCrs.length; i++){
+        var test = "selectCr(" + n + ")";
+        if(crsBarCrs[i].getAttribute("onclick") == test){
+            crsBarCrs[i].setAttribute("class", "nav-link active");
+        }
+    }
 }
+//Select a course complete
 
+//GUI part of adding a semester
 function addSemester(){
     alert("Add a semester, input the name in the field and then press enter. To cancel input a blank space and then press enter");
     var sems = document.getElementById("semesters");
@@ -194,7 +217,9 @@ function addSemester(){
         }
     }
 }
+//Add semester complete
 
+//GUI part of adding a course
 function addCourse(){
     alert("Add a course, input the name in the field and then press enter. To cancel input a blank space and then press enter");
     var crs = document.getElementById("courses");
@@ -214,28 +239,31 @@ function addCourse(){
         }
     }
 }
+//Add course complete
 
-
+//GUI part of removing a semester
 function removeSemester(){
     alert("About to delete a semester, to cancel input a blank space and then press Enter");  
     var sems = document.getElementById("semesters");
-    sems.innerHTML += '<li class="nav-item"><input type="text" id="del"></li>';
+    sems.innerHTML += '<li class="nav-item"><input type="text" id="delSem"></li>';
     
-    document.getElementById('del').onkeypress = function(e){
+    document.getElementById('delSem').onkeypress = function(e){
         if (!e) e = window.event;
         var keyCode = e.keyCode || e.which;
         if (keyCode == '13'){
           // Enter pressed
-          var inptVal = document.getElementById("del").value; 
+          var inptVal = document.getElementById("delSem").value; 
           if(inptVal == ""){
             return;
           }  
-          sems.innerHTML = sems.innerHTML.replace('<li class="nav-item"><input type="text" id="del"></li>','');
+          sems.innerHTML = sems.innerHTML.replace('<li class="nav-item"><input type="text" id="delSem"></li>','');
           stu.removeSemester(inptVal);
         }
     }
 }
+//Remove semester complete
 
+//GUI part of removing a course
 function removeCourse(){
     alert("About to delete a semester, to cancel input a blank space and then press Enter");  
     var crs = document.getElementById("courses");
@@ -255,37 +283,89 @@ function removeCourse(){
         }
     }
 }
+//Remove course complete
+
+
+
+
 
 //Test
 currSem = new Semester("Winter 2020");
 currCourse = new Course("COMP 2002");
-currCourse.addMarkedWork("Assignment 1", 100, 90, 1);
-currCourse.addMarkedWork("Assignment 2", 100, 80, 1);
-currCourse.addMarkedWork("Assignment 3", 100, 70, 1);
-currCourse.addMarkedWork("Assignment 4", 100, 60, 1);
-currCourse.addMarkedWork("Assignment 5", 100, 50, 1);
-currCourse.addMarkedWork("Assignment 6", 100, 40, 1);
-currCourse.addMarkedWork("Assignment 7", 100, 30, 1);
+currCourse.addMarkedWork("Assignment 1", 100, 30, 10);
+currCourse.addMarkedWork("Assignment 2", 100, 40, 10);
+currCourse.addMarkedWork("Assignment 3", 100, 50, 5);
+currCourse.addMarkedWork("Assignment 4", 100, 90, 4);
+currCourse.addMarkedWork("Assignment 5", 100, 50, 25);
+currCourse.addMarkedWork("Assignment 6", 100, 60, 50);
+currCourse.addMarkedWork("Assignment 7", 100, 70, 1);
 
 function updateGradesTable(){
     var tb = document.getElementById("gradesTable");
     tb.innerHTML = '';
     for(var i = 0; i < currCourse.markedStuff.length; i++){
-        var currMW = currCourse.markedStuff[i];
-        tb.innerHTML += '<tr><td>' + currMW.name + '</td><td>' + currMW.totalMarks + '</td><td>' + currMW.marksReceived + '</td><td>' + currMW.weightage + '</td><td>' + currMW.weightageReceived + '</td><td>' + '<button>remove</button>' + '</td></tr>';
+        var currM = currCourse.markedStuff[i];
+        tb.innerHTML += currCourse.getMarkedWorkInfo(currM);
     }
     generateGraph();
 }
 
+
+function modifyMarkedWork(name){
+    var tb = document.getElementById("gradesTable");
+    
+    for(var i = 0; i < currCourse.markedStuff.length; i++){
+        var currMW = currCourse.markedStuff[i];
+        if (currMW.name == name){
+            var inp1 = '<input type=text id="modifyMWName" value="' + currMW.name + '">';
+            var inp2 = '<input type=text id="modifyMWTTL" value="' + currMW.totalMarks + '">';
+            var inp3 = '<input type=text id="modifyMWMarkRec" value="' + currMW.marksReceived + '">';
+            var inp4 = '<input type=text id="modifyMWWeightage" value="' + currMW.weightage + '">';
+            var inp5 = '<input type=text id="modifyMWWeightRec" value="' + currMW.weightageReceived + '">';
+            var info = '<tr><td>' + inp1 + '</td><td>' + inp2 + '</td><td>' + inp3 + '</td><td>' + inp4 + '</td><td>' + inp5 + '</td><td>' + '<button onclick="addModifiedMarkedWork(\'' + currMW.name + '\')">Add</button><button onclick="removeMarkedWork(\'' + currMW.name + '\')">Remove</button>' + '</td></tr>';
+            var newRow = tb.innerHTML.replace(currCourse.getMarkedWorkInfo(currMW),info);
+            tb.innerHTML = newRow;
+        }
+    }
+}
+
+function addModifiedMarkedWork(name){
+    var tb = document.getElementById("gradesTable");
+    
+    for(var i = 0; i < currCourse.markedStuff.length; i++){
+        var currMW = currCourse.markedStuff[i];
+        if (currMW.name == name){
+            currMW.name = document.getElementById("modifyMWName").value;
+            currMW.totalMarks = document.getElementById("modifyMWTTL").value;
+            currMW.marksReceived = document.getElementById("modifyMWMarkRec").value;
+            currMW.weightage = document.getElementById("modifyMWWeightage").value;
+            currMW.weightageReceived = document.getElementById("modifyMWWeightRec").value;
+        }
+    }
+    updateGradesTable();
+}
+
+function removeMarkedWork(name){
+    var info;
+    currCourse.markedStuff.forEach(function(e){
+        if(e.name == name){
+            info = currCourse.getMarkedWorkInfo(e);
+        }
+    });
+    currCourse.removeMarkedStuff(name);
+    updateGradesTable();
+}
 
 
 function generateGraph(){
       var ctx = document.getElementById("myChart");
     var l = [];
     var m = [];
+    var w = [];
         currCourse.markedStuff.forEach(function(e){
             l.push(e.name);
             m.push(e.marksReceived);
+            w.push(e.weightage);
         })
       var myChart = new Chart(ctx, {
         type: 'line',
@@ -313,6 +393,45 @@ function generateGraph(){
           }
         }
       });
+    
+    ctx = document.getElementById("myPie")
+    
+    var data = {
+    labels: l,
+      datasets: [
+        {
+            fill: true,
+            backgroundColor: [
+                'black',
+                'teal', 
+                'yellow',
+                'blue',
+                'red',
+                'pink',
+                'brown'],
+            data: w,
+        }
+    ]
+};
+
+    // Notice the rotation from the documentation.
+
+    var options = {
+            title: {
+                      display: true,
+                      text: 'What happens when you lend your favorite t-shirt to a girl ?',
+                      position: 'top'
+                  },
+            rotation: -0.7 * Math.PI
+    };
+
+
+    // Chart declaration:
+    var myBarChart = new Chart(ctx, {
+        type: 'pie',
+        data: data,
+        options: options
+    });
 }
 
 function test(){
